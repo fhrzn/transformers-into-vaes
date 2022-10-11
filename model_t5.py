3654993f60f9f2b5116324862c7d27c8e3351a1d
affand20 <fahri.lafa@gmail.com>
2022-10-11 12:46:36 +0300

    add some fix

--- model_t5.py
+++ model_t5.py
@@ -165,7 +165,7 @@ class T5VAE(LightningModule):
         return loss
 
     def validation_epoch_end(self, outputs):
-        ppl, nll, elbo, rec, kl, mi, au = calc_all(self, self.trainer.val_dataloaders)
+        ppl, nll, elbo, rec, kl, mi, au = calc_all(self, self.trainer.val_dataloaders[0])
         self.log("val_ppl", ppl)
         self.log("val_nll", nll)
         self.log("val_elbo", elbo)
